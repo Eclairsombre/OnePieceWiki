83f50721e3ac2bf3577e2a3f8607ee46266d1389
VirisOnGithub <clement.reniers@etu.univ-lyon1.fr>
2024-01-21 17:26:27 +0100

    cut into functions the HomePage Component

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -15,92 +15,30 @@ function HomePage() {
   const [crew, setCrew] = useState([]);
   const [character, setCharacter] = useState([]);
   const [boat, setBoat] = useState([]);
-  const [locate, setLocate] = useState([]);
+  const [locate, setLocate] = useState([]);  
 
-  useEffect(() => {
-    var requestURL = "https://api.api-onepiece.com/v2/arcs/fr";
-    var request = new XMLHttpRequest();
-    request.open("GET", requestURL);
-    request.responseType = "json";
-    request.send();
-    request.onload = function () {
-      const temp = request.response;
-      setArc(temp);
-    };
-  }, []);
+  getCharacters(setCharacter);
+  getCrew(setCrew);
+  getHaki(setHaki);
+  getMovie(setMovie);
+  getBoat(setBoat);
+  getArc(setArc);
+  getFruit(setFruit);
+  getTome(setTome);
+  getEpisode(setEpisode);
+  getLocate(setLocate);
+  
 
-  useEffect(() => {
-    var requestURL = "https://api.api-onepiece.com/v2/fruits/fr";
-    var request = new XMLHttpRequest();
-    request.open("GET", requestURL);
-    request.responseType = "json";
-    request.send();
-    request.onload = function () {
-      const temp = request.response;
-      setFruit(temp);
-    };
-  }, []);
+  return (
+    <div>
+      <Header />
+      <h2>Characters</h2>
+      <CharacterCard characters={character} />
+    </div>
+  );
+}
 
-  useEffect(() => {
-    var requestURL = "https://api.api-onepiece.com/v2/tomes/fr";
-    var request = new XMLHttpRequest();
-    request.open("GET", requestURL);
-    request.responseType = "json";
-    request.send();
-    request.onload = function () {
-      const temp = request.response;
-      setTome(temp);
-    };
-  }, []);
-
-  useEffect(() => {
-    var requestURL = "https://api.api-onepiece.com/v2/episodes/fr";
-    var request = new XMLHttpRequest();
-    request.open("GET", requestURL);
-    request.responseType = "json";
-    request.send();
-    request.onload = function () {
-      const temp = request.response;
-      setEpisode(temp);
-    };
-  }, []);
-
-  useEffect(() => {
-    var requestURL = "https://api.api-onepiece.com/v2/movies/fr";
-    var request = new XMLHttpRequest();
-    request.open("GET", requestURL);
-    request.responseType = "json";
-    request.send();
-    request.onload = function () {
-      const temp = request.response;
-      setMovie(temp);
-    };
-  }, []);
-
-  useEffect(() => {
-    var requestURL = "https://api.api-onepiece.com/v2/hakis/fr";
-    var request = new XMLHttpRequest();
-    request.open("GET", requestURL);
-    request.responseType = "json";
-    request.send();
-    request.onload = function () {
-      const temp = request.response;
-      setHaki(temp);
-    };
-  }, []);
-
-  useEffect(() => {
-    var requestURL = "https://api.api-onepiece.com/v2/crews/fr";
-    var request = new XMLHttpRequest();
-    request.open("GET", requestURL);
-    request.responseType = "json";
-    request.send();
-    request.onload = function () {
-      const temp = request.response;
-      setCrew(temp);
-    };
-  }, []);
-
+function getCharacters(setCharacter){
   useEffect(() => {
     var requestURL = "https://api.api-onepiece.com/v2/characters/fr";
     var request = new XMLHttpRequest();
@@ -112,7 +50,51 @@ function HomePage() {
       setCharacter(temp);
     };
   }, []);
+}
 
+function getCrew(setCrew){
+  useEffect(() => {
+    var requestURL = "https://api.api-onepiece.com/v2/crews/fr";
+    var request = new XMLHttpRequest();
+    request.open("GET", requestURL);
+    request.responseType = "json";
+    request.send();
+    request.onload = function () {
+      const temp = request.response;
+      setCrew(temp);
+    };
+  }, []);
+}
+
+function getHaki(setHaki){
+  useEffect(() => {
+    var requestURL = "https://api.api-onepiece.com/v2/hakis/fr";
+    var request = new XMLHttpRequest();
+    request.open("GET", requestURL);
+    request.responseType = "json";
+    request.send();
+    request.onload = function () {
+      const temp = request.response;
+      setHaki(temp);
+    };
+  }, []);
+}
+
+function getMovie(setMovie){
+  useEffect(() => {
+    var requestURL = "https://api.api-onepiece.com/v2/movies/fr";
+    var request = new XMLHttpRequest();
+    request.open("GET", requestURL);
+    request.responseType = "json";
+    request.send();
+    request.onload = function () {
+      const temp = request.response;
+      setMovie(temp);
+    };
+  }, []);
+}
+
+function getBoat(setBoat){
   useEffect(() => {
     var requestURL = "https://api.api-onepiece.com/v2/boats/fr";
     var request = new XMLHttpRequest();
@@ -124,7 +106,65 @@ function HomePage() {
       setBoat(temp);
     };
   }, []);
+}
 
+function getArc(setArc){
+  useEffect(() => {
+    var requestURL = "https://api.api-onepiece.com/v2/arcs/fr";
+    var request = new XMLHttpRequest();
+    request.open("GET", requestURL);
+    request.responseType = "json";
+    request.send();
+    request.onload = function () {
+      const temp = request.response;
+      setArc(temp);
+    };
+  }, []);
+}
+
+function getFruit(setFruit){
+  useEffect(() => {
+    var requestURL = "https://api.api-onepiece.com/v2/fruits/fr";
+    var request = new XMLHttpRequest();
+    request.open("GET", requestURL);
+    request.responseType = "json";
+    request.send();
+    request.onload = function () {
+      const temp = request.response;
+      setFruit(temp);
+    };
+  }, []);
+}
+
+function getTome(setTome){
+  useEffect(() => {
+    var requestURL = "https://api.api-onepiece.com/v2/tomes/fr";
+    var request = new XMLHttpRequest();
+    request.open("GET", requestURL);
+    request.responseType = "json";
+    request.send();
+    request.onload = function () {
+      const temp = request.response;
+      setTome(temp);
+    };
+  }, []);
+}
+
+function getEpisode(setEpisode){
+  useEffect(() => {
+    var requestURL = "https://api.api-onepiece.com/v2/episodes/fr";
+    var request = new XMLHttpRequest();
+    request.open("GET", requestURL);
+    request.responseType = "json";
+    request.send();
+    request.onload = function () {
+      const temp = request.response;
+      setEpisode(temp);
+    };
+  }, []);
+}
+
+function getLocate(setLocate){
   useEffect(() => {
     var requestURL = "https://api.api-onepiece.com/v2/locates/fr";
     var request = new XMLHttpRequest();
@@ -136,14 +176,6 @@ function HomePage() {
       setLocate(temp);
     };
   }, []);
-
-  return (
-    <div>
-      <Header />
-      <h2>Characters</h2>
-      <CharacterCard characters={character} />
-    </div>
-  );
 }
 
 export default HomePage;
