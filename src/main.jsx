83cdff0627e8e612055c01d671431929bec024cc
VirisOnGithub <clement.reniers@etu.univ-lyon1.fr>
2024-01-23 09:44:24 +0100

    Added react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import ErrorPage from "./ErrorPage.jsx";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 import "./index.css";
 import HomePage from "./HomePage.jsx";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HomePage />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
