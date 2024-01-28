7795c62a8989a7dc5e61adf21d31a9e8df79b30d
VirisOnGithub <clement.reniers@etu.univ-lyon1.fr>
2024-01-28 18:25:20 +0100

    corrected date display

--- src/TomePage.jsx
+++ src/TomePage.jsx
@@ -3,6 +3,8 @@ import Header from "./Header";
 import "./CharacterPage.css";
 
 function TomePage({ tome, setSelectedTome }) {
+  let date = tome.tome_french_date_publish;
+  let date2 = tome.tome_japan_date_publish;
   return (
     <div className="character-page-container">
       <Header />
@@ -21,8 +23,8 @@ function TomePage({ tome, setSelectedTome }) {
           </div>
           <div className="assets">
             <p>Tome number : {tome.tome_number}</p>
-            <p>Publish date in Japan : {tome.tome_japan_date_publish}</p>
-            <p>Publish date in France : {tome.tome_french_date_publish}</p>
+            <p>Publish date in Japan : {date2.slice(8,10) + "/" + date2.slice(5,7) + "/" + date2.slice(0,4)}</p>
+            <p>Publish date in France : {date.slice(8,10) + "/" + date.slice(5,7) + "/" + date.slice(0,4)}</p>
           </div>
         </div>
       </div>
